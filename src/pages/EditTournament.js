b6caa2d52d955e088d27de32828ead5872ddeba5
vacenric <vacenric@fit.cvut.cz>
2023-10-25 13:30:01 +0200

    Finish edit tournament page without formating

--- src/pages/EditTournament.js
+++ src/pages/EditTournament.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useForm, Controller } from "react-hook-form";
-import "../styles/AddTournament.css";
+// import "../styles/EditTournament.css";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import Select from "react-select";
@@ -22,6 +22,16 @@ export default function EditTournament({
   const { errors } = formState;
   const [editedTournament, setEditedTournament] = useState();
   const [editName, setEditName] = useState(false);
+  const [editDate, setEditDate] = useState(false);
+  const [editCity, setEditCity] = useState(false);
+  const [editAreal, setEditAreal] = useState(false);
+  const [editCapacity, setEditCapacity] = useState(false);
+  const [editPrice, setEditPrice] = useState(false);
+  const [editStart, setEditStart] = useState(false);
+  const [editOrganizer, setEditOrganizer] = useState(false);
+  const [editCategory, setEditCategory] = useState(false);
+  const [editLevel, setEditLevel] = useState(false);
+  const [editLink, setEditLink] = useState(false);
   const [myId, setmyId] = useState();
 
   // get all tournaments and filter by id the one to be edited
@@ -46,6 +56,13 @@ export default function EditTournament({
 
   const onSubmit = (data) => {
     data.user_id = jwt_decode(token.access_token).sub;
+    if (data.level) {
+      data.level = data.level.label;
+    }
+    if (data.category) {
+      data.category = data.category.label;
+    }
+
     fetch(`${apiUrl}/update/${localStorage.getItem("editId")}/`, {
       method: "PUT",
       headers: {
@@ -71,20 +88,21 @@ export default function EditTournament({
   ];
 
   return (
-    <div className="AddTournament--main">
+    <div className="EditTournament--main">
       <h1>Upravit turnaj:</h1>
-      <div className="AddTournament--form-element">
+      <div className="EditTournament--form-element">
         <label>Jméno turnaje</label>
         {!editName ? (
-          <h3>
+          <p>
             {editedTournament && editedTournament.name}{" "}
             <button onClick={() => setEditName(true)}>editovat</button>
-          </h3>
+          </p>
         ) : (
           <form onSubmit={handleSubmit(onSubmit)}>
             <input
               type="text"
               name="name"
+              defaultValue={editedTournament && editedTournament.name}
               {...register("name", {
                 required: {
                   value: true,
@@ -96,16 +114,317 @@ export default function EditTournament({
           </form>
         )}
       </div>
-      <p>{editedTournament && editedTournament.date}</p>
-      <p>{editedTournament && editedTournament.city}</p>
-      <p>{editedTournament && editedTournament.areal}</p>
-      <p>{editedTournament && editedTournament.capacity}</p>
-      <p>{editedTournament && editedTournament.price}</p>
-      <p>{editedTournament && editedTournament.start}</p>
-      <p>{editedTournament && editedTournament.organizer}</p>
-      <p>{editedTournament && editedTournament.category}</p>
-      <p>{editedTournament && editedTournament.level}</p>
-      <p>{editedTournament && editedTournament.link}</p>
+
+      <div className="EditTournament--form-element">
+        <label>Datum</label>
+        {!editDate ? (
+          <p>
+            {editedTournament && editedTournament.date}
+            <button onClick={() => setEditDate(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="date"
+              name="date"
+              defaultValue={editedTournament && editedTournament.date}
+              {...register("date", {
+                required: {
+                  value: true,
+                  message: "Zadejte datum turnaje",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Město</label>
+        {!editCity ? (
+          <p>
+            {editedTournament && editedTournament.city}{" "}
+            <button onClick={() => setEditCity(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="city"
+              defaultValue={editedTournament && editedTournament.city}
+              {...register("city", {
+                required: {
+                  value: true,
+                  message: "Zadejte jméno turnaje",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Areál</label>
+        {!editAreal ? (
+          <p>
+            {editedTournament && editedTournament.areal}{" "}
+            <button onClick={() => setEditAreal(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="areal"
+              defaultValue={editedTournament && editedTournament.areal}
+              {...register("areal", {
+                required: {
+                  value: true,
+                  message: "Zadejte jméno turnaje",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Kapacita</label>
+        {!editCapacity ? (
+          <p>
+            {editedTournament && editedTournament.capacity}{" "}
+            <button onClick={() => setEditCapacity(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="capacity"
+              defaultValue={editedTournament && editedTournament.capacity}
+              {...register("capacity", {
+                required: {
+                  value: true,
+                  message: "Zadejte město, kde se turnaj koná",
+                },
+
+                pattern: {
+                  value: /^\d+$/,
+                  message: "Zadaná hodnota není validní",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Startovné</label>
+        {!editPrice ? (
+          <p>
+            {editedTournament && editedTournament.price}{" "}
+            <button onClick={() => setEditPrice(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="price"
+              defaultValue={editedTournament && editedTournament.price}
+              {...register("price", {
+                required: {
+                  value: true,
+                  message: "Zadejte město, kde se turnaj koná",
+                },
+
+                pattern: {
+                  value: /^\d+$/,
+                  message: "Zadaná hodnota není validní",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Začátek</label>
+        {!editStart ? (
+          <p>
+            {editedTournament && editedTournament.start}{" "}
+            <button onClick={() => setEditStart(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="start"
+              placeholder="HH:mm"
+              defaultValue={editedTournament && editedTournament.start}
+              pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+              {...register("start", {
+                required: {
+                  value: true,
+                  message: "Zadejte čas začátku turnaje ",
+                },
+              })}
+              onChange={(e) => {
+                const input = e.target;
+                const value = input.value;
+                if (/^\d{1,2}:\d{0,2}$/.test(value)) {
+                  const [hours, minutes] = value.split(":");
+                  if (
+                    parseInt(hours, 10) <= 23 &&
+                    parseInt(minutes, 10) <= 59
+                  ) {
+                    input.setCustomValidity("");
+                  } else {
+                    input.setCustomValidity("Invalid time");
+                  }
+                } else {
+                  input.setCustomValidity("Invalid time");
+                }
+              }}
+            />
+
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Organizátor</label>
+        {!editOrganizer ? (
+          <p>
+            {editedTournament && editedTournament.organizer}{" "}
+            <button onClick={() => setEditOrganizer(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="organizer"
+              defaultValue={editedTournament && editedTournament.organizer}
+              {...register("organizer", {
+                required: {
+                  value: true,
+                  message: "Zadejte jméno turnaje",
+                },
+              })}
+            />
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Kategorie</label>
+        {!editCategory ? (
+          <p>
+            {editedTournament && editedTournament.category}{" "}
+            <button onClick={() => setEditCategory(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="category"
+              control={control}
+              rules={{ required: "Vyberte úroveň" }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={categoryOptions}
+                  placeholder={"Vyberte kategorii"}
+                  styles={{
+                    // Add custom styles for the border
+                    control: (base, state) => ({
+                      ...base,
+                      border: "1px solid #e6e6e6",
+                      fontFamily: "Montserrat, sans-serif",
+                      fontSize: "18px",
+                      width: "100%",
+                      backgroundColor: "#f7f7f7",
+                      height: "40px",
+                      borderRadius: "10px",
+                    }),
+                  }}
+                />
+              )}
+            />
+
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Úroveň</label>
+        {!editLevel ? (
+          <p>
+            {editedTournament && editedTournament.level}{" "}
+            <button onClick={() => setEditLevel(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="level"
+              control={control}
+              rules={{ required: "Vyberte úroveň" }}
+              render={({ field }) => (
+                <Select
+                  {...field}
+                  options={levelOptions}
+                  placeholder={"Vyberte úroveň"}
+                  styles={{
+                    // Add custom styles for the border
+                    control: (base, state) => ({
+                      ...base,
+                      border: "1px solid #e6e6e6",
+                      fontFamily: "Montserrat, sans-serif",
+                      fontSize: "18px",
+                      width: "100%",
+                      backgroundColor: "#f7f7f7",
+                      height: "40px",
+                      borderRadius: "10px",
+                    }),
+                  }}
+                />
+              )}
+            />
+
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
+
+      <div className="EditTournament--form-element">
+        <label>Odkaz</label>
+        {!editLink ? (
+          <p>
+            {editedTournament && editedTournament.link}{" "}
+            <button onClick={() => setEditLink(true)}>editovat</button>
+          </p>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="text"
+              name="link"
+              defaultValue={editedTournament && editedTournament.link}
+              {...register("link", {
+                required: {
+                  value: true,
+                  message: "Zadejte odkaz na turnaj",
+                },
+              })}
+            />
+
+            <button type="submit">uložit</button>
+          </form>
+        )}
+      </div>
 
       {/* <DevTool control={control} /> */}
     </div>
